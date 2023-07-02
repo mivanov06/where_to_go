93881dad46bce37d1ecf01eef626b617f1432e77
mivanov06 <mivanov06@yandex.ru>
2023-07-02 16:32:49 +1000

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from places.models import Place
+from places.models import Place, Image
 
 
 # Register your models here.
@@ -8,3 +8,8 @@ from places.models import Place
 @admin.register(Place)
 class AdminPlace(admin.ModelAdmin):
     list_display = ('title',)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('place', 'position')
--- places/models.py
+++ places/models.py
@@ -10,3 +10,12 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField('Картинка', null=True)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Место', related_name='images')
+    position = models.PositiveIntegerField('Позиция', default=0)
+
+    def __str__(self):
+        return f'{self.position} - {self.place.title}'
