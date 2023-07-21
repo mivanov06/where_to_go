7486fd122c79700326761dd2abb4125f789f9182
mivanov06 <mivanov06@yandex.ru>
2023-07-21 18:01:48 +1000

    Add model Image

--- places/admin.py
+++ places/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from places.models import Place
+from places.models import Place, Image
 
 
 @admin.register(Place)
 class AdminPlace(admin.ModelAdmin):
     list_display = ('title',)
+
+
+@admin.register(Image)
+class AdminImage(admin.ModelAdmin):
+    list_display = ('image', 'place', 'position')
--- places/models.py
+++ places/models.py
@@ -12,3 +12,15 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField('Картинка', null=True)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Место', related_name='images')
+    position = models.PositiveIntegerField('Позиция', default=0)
+
+    class Meta(object):
+        ordering = ['position']
+
+    def __str__(self):
+        return f'{self.position} - {self.place.title}'
