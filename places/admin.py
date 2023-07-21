69b4a31ce622d7157b77d67307eea7c24db15b81
mivanov06 <mivanov06@yandex.ru>
2023-07-21 17:55:08 +1000

    Add model Place

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from places.models import Place
+
+
+@admin.register(Place)
+class AdminPlace(admin.ModelAdmin):
+    list_display = ('title',)
