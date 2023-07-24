e580a12dc50fec0eb04ace445447ed6da2e84647
Maksim Ivanov <77063936+mivanov06@users.noreply.github.com>
2023-07-25 05:04:11 +1000

    refactored Command load_place

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -14,7 +14,25 @@ class Command(BaseCommand):
             'file_url', type=str, help='Ссылка на данные с местом на карте'
         )
 
-    def get_place(self, content):
+    def download_image(self, index, image_url, place):
+        image_name = image_url.split('/')[-1]
+        image_response = requests.get(image_url)
+        image_response.raise_for_status()
+        image = ContentFile(
+            image_response.content,
+            name=image_name
+        )
+        image, _ = Image.objects.update_or_create(
+            place=place,
+            image=image,
+            position=index,
+        )
+
+    def handle(self, *args, **kwargs):
+        file_url = kwargs['file_url']
+        response = requests.get(file_url)
+        response.raise_for_status()
+        content = response.json()
         place, _ = Place.objects.update_or_create(
             title=content['title'],
             description_short=content.get('description_short', ''),
@@ -22,28 +40,7 @@ class Command(BaseCommand):
             longitude=content['coordinates']['lng'],
             latitude=content['coordinates']['lat']
         )
-        return place
 
-    def get_images(self, content, place):
         images_url = content.get('imgs', [])
         for index, image_url in enumerate(images_url, 1):
-            image_name = image_url.split('/')[-1]
-            image_response = requests.get(image_url)
-            image_response.raise_for_status()
-            image = ContentFile(
-                image_response.content,
-                name=image_name
-            )
-            image, _ = Image.objects.update_or_create(
-                place=place,
-                image=image,
-                position=index,
-            )
-
-    def handle(self, *args, **kwargs):
-        file_url = kwargs['file_url']
-        response = requests.get(file_url)
-        response.raise_for_status()
-        content = response.json()
-        place = self.get_place(content)
-        self.get_images(content, place)
+            self.download_image(index, image_url, place)
